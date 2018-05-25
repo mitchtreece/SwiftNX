19a13f564f4fc00ea1ea161a73a1871fa32febc9
Mitch Treece <mitchtreece@me.com>
2018-05-25 11:56:34 -0700

    Trying -fpic (positional independent code) to suppress relocation build error

--- src/main.swift
+++ src/main.swift
@@ -4,27 +4,106 @@ import libnx
 @_silgen_name("printf")
 func printf(_ format: StaticString, _ parameters: StaticString ...)
 
-@_silgen_name("swift_main")
-func swift_main() -> Int {
+struct NXApplet {
 
-    var console = PrintConsole()
+    static func main(_ loop: ()->()) {
 
-    gfxInitDefault()
-
-    consoleInit(&console)
-    consoleSelect(&console)
-
-    printf("Hello, world!")
-
-    while(appletMainLoop()) {
-
-        gfxFlushBuffers()
-		gfxSwapBuffers()
-        gfxWaitForVsync()
+        while(appletMainLoop()) {
+            loop()
+        }
 
     }
 
-    gfxExit()
-    return 0
+}
+
+struct NXGfx {
+
+    func flush() {
+        gfxFlushBuffers()
+    }
+
+    func swap() {
+        gfxSwapBuffers()
+    }
+
+    func waitForVsync() {
+        gfxWaitForVsync()
+    }
+
+    func exit(_ code: Int) -> Int {
+
+        gfxExit()
+        return code
+
+    }
 
 }
+//
+// struct NXConsole {
+//
+//     private var _console: PrintConsole
+//
+//     init() {
+//
+//         _console = PrintConsole()
+//         _init()
+//
+//     }
+//
+//     private mutating func _init() {
+//         consoleInit(&_console)
+//     }
+//
+//     mutating func select() {
+//         consoleSelect(&_console)
+//     }
+//
+//     func print(_ string: StaticString) {
+//         printf(string)
+//     }
+//
+// }
+
+@_silgen_name("swift_main")
+func swift_main() -> Int {
+
+    let gfx = NXGfx()
+
+    // var console = NXConsole()
+    // console.select()
+    // console.print("Hello, world!")
+
+    NXApplet.main {
+
+        gfx.flush()
+        gfx.swap()
+        gfx.waitForVsync()
+
+    }
+
+    return gfx.exit(0)
+
+}
+
+// @_silgen_name("swift_main")
+// func swift_main() -> Int {
+//
+//     gfxInitDefault()
+//
+//     var console = PrintConsole()
+//     consoleInit(&console)
+//     consoleSelect(&console)
+//     printf("Hello, world!")
+//
+//     while(appletMainLoop()) {
+//
+//         gfxFlushBuffers()
+//         gfxSwapBuffers()
+//         gfxWaitForVsync()
+//
+//     }
+//
+//     gfxExit()
+//     return 0
+//
+// }
--- src/swift.h
+++ src/swift.h
@@ -1,6 +1,28 @@
 int swift_main();
 
+// Base
+
 void _T0s12StaticStringVN() {};
 void _T0s27_allocateUninitializedArraySayxG_BptBwlF() {};
 void swift_bridgeObjectRetain() {};
 void swift_bridgeObjectRelease() {};
+
+// NXApplet + struct
+
+void _swift_retain() {};
+void _swift_release() {};
+void _swift_slowAlloc() {};
+void _swift_slowDealloc() {};
+
+// NXConsole + mutating
+
+void swift_getForeignTypeMetadata() {};
+void swift_getFunctionTypeMetadata2() {};
+void _T0s5Int32VN() {};
+void _T0SbN() {};
+void _T0s6UInt16VN() {};
+void _T0SQMa() {};
+void _T0SqMa() {};
+void _T0s6UInt32VN() {};
+void _T0SpMa() {};
+void _T0SvN() {};
