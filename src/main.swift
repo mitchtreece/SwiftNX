37b306316d7c5e50aa90b21efcb7481cc4c0b3ff
Mitch Treece <mitchtreece@me.com>
2018-05-28 17:00:16 -0700

    Working on getting gfx working from swift wrapper class

--- src/main.swift
+++ src/main.swift
@@ -14,10 +14,12 @@ import libnx
 
 include "lib/SwiftNX.swift"
 
+// MARK: GFX
+
 @_silgen_name("swift_main")
 func swift_main() -> Int {
 
-    gfxInitDefault()
+    let gfx = NXGfx()
 
     var console = PrintConsole()
     consoleInit(&console)
@@ -25,14 +27,105 @@ func swift_main() -> Int {
     printf("Hello, swift!")
 
     while(appletMainLoop()) {
-
-        gfxFlushBuffers()
-        gfxSwapBuffers()
-        gfxWaitForVsync()
-
+        gfx.clean()
     }
 
-    gfxExit()
-    return 0
+    return gfx.exit(0)
 
 }
+
+// MARK: Applet + console
+
+// @_silgen_name("swift_main")
+// func swift_main() -> Int {
+//
+//     gfxInitDefault()
+//
+//     let console = NXConsole()
+//     console.print("Hello, swift!")
+//
+//     NXApplet.main {
+//
+//         gfxFlushBuffers()
+//         gfxSwapBuffers()
+//         gfxWaitForVsync()
+//
+//     }
+//
+//     gfxExit()
+//     return 0
+//
+// }
+
+// MARK: Console only - NOT WORKING (no console text is drawn)
+
+// @_silgen_name("swift_main")
+// func swift_main() -> Int {
+//
+//     gfxInitDefault()
+//
+//     let console = NXConsole()
+//     console.print("Hello, swift!")
+//
+//     while(appletMainLoop()) {
+//
+//         gfxFlushBuffers()
+//         gfxSwapBuffers()
+//         gfxWaitForVsync()
+//
+//     }
+//
+//     gfxExit()
+//     return 0
+//
+// }
+
+// MARK: Applet only - WORKING (console text looks weird)
+
+// @_silgen_name("swift_main")
+// func swift_main() -> Int {
+//
+//     gfxInitDefault()
+//
+//     var console = PrintConsole()
+//     consoleInit(&console)
+//     consoleSelect(&console)
+//     printf("Hello, swift!")
+//
+//     NXApplet.main {
+//
+//         gfxFlushBuffers()
+//         gfxSwapBuffers()
+//         gfxWaitForVsync()
+//
+//     }
+//
+//     gfxExit()
+//     return 0
+//
+// }
+
+// MARK: Template - WORKING
+
+// @_silgen_name("swift_main")
+// func swift_main() -> Int {
+//
+//     gfxInitDefault()
+//
+//     var console = PrintConsole()
+//     consoleInit(&console)
+//     consoleSelect(&console)
+//     printf("Hello, swift!")
+//
+//     while(appletMainLoop()) {
+//
+//         gfxFlushBuffers()
+//         gfxSwapBuffers()
+//         gfxWaitForVsync()
+//
+//     }
+//
+//     gfxExit()
+//     return 0
+//
+// }
